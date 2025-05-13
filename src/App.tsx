1515168e2701ea39d0f509e45f2ba179a32ce505
erincfuller <74694020+erincfuller@users.noreply.github.com>
2025-05-13 14:33:28 -0400

    Prayer

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import UnitedRentals from "./pages/UnitedRentals";
+import CommonPass from "./pages/CommonPass";
+import FindFood from "./pages/FindFood";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects/united-rentals" element={<UnitedRentals />} />
+          <Route path="/projects/common-pass" element={<CommonPass />} />
+          <Route path="/projects/find-food" element={<FindFood />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
